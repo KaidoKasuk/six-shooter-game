d620cc5a3bd40e33337b60885aa6c9246d30977c
KaidoKasuk <kaido.kasuk@voco.ee>
2026-03-05 13:47:06 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import Player from "./Uppersection/Player";
+import Login from "./login/Login";
 function App() {
   return (
     <>
+      <Login></Login>
       <Player></Player>
     </>
   );
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,3 +1,35 @@
-function Login() {}
+function Login() {
+  return (
+    <form>
+      <div>
+        <label htmlFor="Email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={emailValidateHandler}
+        />
+      </div>
+      <div
+        className={`control ${passwordState.isValid === false ? "invalid" : ""}`}
+      >
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={passwordValidateHandler}
+        />
+      </div>
+      <div className="actions">
+        <Button type="submit" disabled={!formIsValid}>
+          Login
+        </Button>
+      </div>
+    </form>
+  );
+}
 
 export default Login;
